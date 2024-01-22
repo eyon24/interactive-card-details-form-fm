955d33246e255844be73f0df25bbe40f3aa645a7
Eric Yon <ericyon24@gmail.com>
2024-01-22 07:11:56 -0600

    commit

--- myScript.js
+++ myScript.js
@@ -1,9 +1,13 @@
+const infoContainer = document.getElementById("info-component");
+const confirmation = document.getElementById("confirmation");
 const form = document.getElementById("form");
 const cardName = document.getElementById("cardName");
 const cardNumber = document.getElementById("cardNumber");
 const mm = document.getElementById("mm");
 const yy = document.getElementById("yy");
 const cvc = document.getElementById("cvc");
+const confirmBtn = document.getElementById("confirmBtn");
+const continueBtn = document.getElementById("continueBtn");
 
 form.elements["cardName"].addEventListener("keyup", function (e) {
   if (e.target.value === "") {
@@ -63,18 +67,19 @@ const setError = (element, message) => {
   errorDisplay.innerText = message;
 };
 
-const validate = () => {
+const validate = (e) => {
   var nameValue = cardName.value;
   var cardNumberValue = cardNumber.value;
   var mmValue = mm.value;
   var yyValue = yy.value;
   var cvcValue = cvc.value;
-  var regExp = new RegExp(/[a-zA-Z]/g);
-  var regExpCard = new RegExp();
+  var regExp = new RegExp(/[a-zA-Z-,]/g);
+  var valid = true;
 
   // Validate Name
   if (nameValue === "" || nameValue === null) {
     setError(cardName, "Please Enter a Name");
+    valid = false;
   } else {
     setSuccess(cardName);
   }
@@ -82,10 +87,13 @@ const validate = () => {
   // Validate Card Number
   if (cardNumberValue === "" || cardNumberValue === null) {
     setError(cardNumber, "Please Enter a Card Number");
+    valid = false;
   } else if (cardNumberValue.length < 16) {
     setError(cardNumber, "Incorrect Card Number");
+    valid = false;
   } else if (regExp.test(cardNumberValue)) {
     setError(cardNumber, "Wrong Format, Numbers Only");
+    valid = false;
   } else {
     setSuccess(cardNumber);
   }
@@ -93,40 +101,64 @@ const validate = () => {
   //Validate MM and YY and CVC
   if (mmValue === "") {
     setError(mm, "Can't Be Blank");
+    valid = false;
   } else if (mmValue.length < 2) {
     setError(mm, "Wrong Format");
+    valid = false;
   } else if (regExp.test(mmValue)) {
     setError(mm, "Numbers Only");
+    valid = false;
   } else if (parseInt(mmValue) > 12 || parseInt(mmValue) < 1) {
     setError(mm, "Check Date");
+    valid = false;
   } else {
     setSuccess(mm);
   }
 
   if (yyValue === "") {
     setError(yy, "Can't Be Blank");
+    valid = false;
   } else if (yyValue.length < 2) {
     setError(yy, "Wrong Format");
+    valid = false;
   } else if (regExp.test(yyValue)) {
     setError(yy, "Numbers Only");
+    valid = false;
   } else if (parseInt(yyValue) < 1) {
     setError(yy, "Check Date");
+    valid = false;
   } else {
     setSuccess(yy);
   }
 
   if (cvcValue === "") {
     setError(cvc, "Can't Be Blank");
+    valid = false;
   } else if (cvcValue.length < 3) {
     setError(cvc, "Wrong Format");
+    valid = false;
   } else if (regExp.test(cvcValue)) {
     setError(cvc, "Numbers Only");
+    valid = false;
   } else {
     setSuccess(cvc);
   }
 
-  return;
+  if (valid) {
+    infoContainer.classList.add("hide");
+    confirmBtn.classList.add("hide");
+
+    confirmation.classList.remove("hide");
+  }
 };
+
+continueBtn.addEventListener("click", () => {
+  // continueBtn.classList.add("hide");
+  confirmation.classList.add("hide");
+
+  infoContainer.classList.remove("hide");
+  confirmBtn.classList.remove("hide");
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
